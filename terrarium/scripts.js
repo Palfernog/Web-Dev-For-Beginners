a4094f5dd0500571cf7e2ed29b811d23e2115e09
preston <prestonyeaman@yahoo.com>
2022-12-03 14:58:27 -0600

    Add function to scripts.js

--- terrarium/scripts.js
+++ terrarium/scripts.js
@@ -1,1 +1,10 @@
-dragElement(document.getElementByClass('plant'))
+dragElement(document.getElementByClass('plant'));
+
+function dragElement(terrariumElement) {
+	//set 4 positions for positioning on the screen
+	let pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
+	terrariumElement.onpointerdown = pointerDrag;
+}
