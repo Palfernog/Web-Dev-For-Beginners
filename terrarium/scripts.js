5493d5c856c6d7a74860f938d82f7f2714433b2b
preston <prestonyeaman@yahoo.com>
2022-12-03 15:30:02 -0600

    Change scripts.js

--- terrarium/scripts.js
+++ terrarium/scripts.js
@@ -1,5 +1,19 @@
-dragElement(document.getElementByClass('plant'));
 
+dragElement(document.getElementById('plant1'));
+dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant4'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
+dragElement(document.getElementById('plant7'));
+dragElement(document.getElementById('plant8'));
+dragElement(document.getElementById('plant9'));
+dragElement(document.getElementById('plant10'));
+dragElement(document.getElementById('plant11'));
+dragElement(document.getElementById('plant12'));
+dragElement(document.getElementById('plant13'));
+dragElement(document.getElementById('plant14'));
+ 
 function dragElement(terrariumElement) {
 	//set 4 positions for positioning on the screen
 	let pos1 = 0,
@@ -7,6 +21,7 @@ function dragElement(terrariumElement) {
 		pos3 = 0,
 		pos4 = 0;
 	terrariumElement.onpointerdown = pointerDrag;
+    
     function pointerDrag(e) {
         e.preventDefault();
         console.log(e);
@@ -15,6 +30,7 @@ function dragElement(terrariumElement) {
         document.onpointermove = elementDrag;
         document.onpointerup = stopElementDrag;
     }
+
     function elementDrag(e) {
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
@@ -24,6 +40,7 @@ function dragElement(terrariumElement) {
         terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
         terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
     }
+
     function stopElementDrag() {
         document.onpointerup = null;
         document.onpointermove = null;
